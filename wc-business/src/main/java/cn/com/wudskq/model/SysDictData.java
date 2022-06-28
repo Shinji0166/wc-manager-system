24a3811559fb6e5e14fceadf3d22e29cb416ea49
chenfangchao <socrates11@foxmail.com>
2022-06-28 17:10:25 +0800

    [add]#新增系统字典表实体模型

---
+++ wc-business/src/main/java/cn/com/wudskq/model/SysDictData.java
@@ -0,0 +1,41 @@
+package cn.com.wudskq.model;
+
+import cn.com.wudskq.common.CreateInfoModel;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author chenfangchao
+ * @title: SysDictData
+ * @projectName wc-manager-system
+ * @description: TODO 系统字典数据表模型
+ * @date 2022/6/28 5:06 PM
+ */
+@ApiModel(value = "系统字典数据表模型")
+@Data
+@TableName("sys_dict_data")
+public class SysDictData extends CreateInfoModel implements Serializable {
+
+    private static final long serialVersionUID = 2686814784445338505L;
+
+    @ApiModelProperty(name = "主键")
+    @TableId("id")
+    private Long id ;
+
+    @ApiModelProperty(name = "字典标签")
+    private String dictLabel;
+
+    @ApiModelProperty(name = "字典值")
+    private  String dictValue;
+
+    @ApiModelProperty(name = "字典类型")
+    private  String dictType;
+
+    @ApiModelProperty(name = "状态(0启用 1禁用)")
+    private  Integer status;
+}
---
+++ wc-business/src/main/java/cn/com/wudskq/model/SysDictType.java
@@ -0,0 +1,43 @@
+package cn.com.wudskq.model;
+
+import cn.com.wudskq.common.CreateInfoModel;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author chenfangchao
+ * @title: SysDictType
+ * @projectName wc-manager-system
+ * @description: TODO 系统字典类型表模型
+ * @date 2022/6/28 5:01 PM
+ */
+@ApiModel(value = "系统字典类型表模型")
+@Data
+@TableName("sys_dict_type")
+public class SysDictType extends CreateInfoModel implements Serializable {
+
+    private static final long serialVersionUID = 8862901131940149379L;
+
+    @ApiModelProperty(name = "主键")
+    private Long id;
+
+    @ApiModelProperty(name = "字典名称")
+    private String label;
+
+    @ApiModelProperty(name = "字典类型名称")
+    private String type;
+
+    @ApiModelProperty(name = "字典分类(0系统字典 1模块字典 2公共字典)")
+    private  Integer classify;
+
+    @ApiModelProperty(name = "父级ID")
+    private Integer pid;
+
+    @ApiModelProperty(name = "状态(0正常 1删除 2禁用)")
+    private Integer status;
+
+}
